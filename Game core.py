c72da88a75d06743c62d09d901c4be6debdbc876
Lewandodev <mlewandowski380@gmail.com>
2022-09-24 19:55:01 +0200

    Basic movement done

--- Game core.py
+++ Game core.py
@@ -10,13 +10,31 @@ x=50
 y=50
 width=30
 height=60
-velocity=5
+velocity=10 #speed of our character
 
 run=True
 while run is True:
     pygame.time.delay(100)
 
     for event in pygame.event.get(): #event is anything that happens "from the user" like using movement keys or clicking
-        if event.type==pygame.QUIT:
+        if event.type==pygame.QUIT:  #thanks to this function we wont get error after exiting game
             run=False
 
+    #movement
+    move_keys=pygame.key.get_pressed()
+    if move_keys[pygame.K_LEFT]:
+        x-=velocity
+    elif move_keys[pygame.K_RIGHT]:
+        x+=velocity
+    elif move_keys[pygame.K_UP]:
+        y-=velocity
+    elif move_keys[pygame.K_DOWN]:
+        y+=velocity
+
+    window.fill((0,0,0))
+    #creating character
+    pygame.draw.rect(window, (0,255,0),(x,y,width,height)) #we put our "character on surface (window) then add parameters
+    pygame.display.update()
+
+
+pygame.quit()
