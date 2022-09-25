86aba7affb343e89bdd2188d24755f6fb0e5ab3f
Lewandodev <mlewandowski380@gmail.com>
2022-09-25 23:55:03 +0200

    Borders

--- Game core.py
+++ Game core.py
@@ -6,8 +6,9 @@ window=pygame.display.set_mode((600,600)) #displaying the window
 
 pygame.display.set_caption("Python Game Project")
 
+screen_border=600
 x=50
-y=50
+y=425
 width=30
 height=60
 velocity=10 #speed of our character
@@ -21,20 +22,22 @@ while run is True:
             run=False
 
     #movement
+    #coordinates of object are stored in top left of the character
     move_keys=pygame.key.get_pressed()
-    if move_keys[pygame.K_LEFT]:
+    if move_keys[pygame.K_LEFT] and x>velocity:
         x-=velocity
-    elif move_keys[pygame.K_RIGHT]:
+    elif move_keys[pygame.K_RIGHT] and x<screen_border-width-velocity:
         x+=velocity
-    elif move_keys[pygame.K_UP]:
+    elif move_keys[pygame.K_UP] and y > velocity:
         y-=velocity
-    elif move_keys[pygame.K_DOWN]:
+    elif move_keys[pygame.K_DOWN] and y < screen_border-height-velocity:
         y+=velocity
 
     window.fill((0,0,0))
     #creating character
     pygame.draw.rect(window, (0,255,0),(x,y,width,height)) #we put our "character on surface (window) then add parameters
     pygame.display.update()
+
 
 
 pygame.quit()
