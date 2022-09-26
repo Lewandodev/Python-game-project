3c0e40adbfd365427f9713be975c56ac996d8b70
Lewandodev <mlewandowski380@gmail.com>
2022-09-26 18:17:14 +0200

    jumping mechanic

--- Game core.py
+++ Game core.py
@@ -13,6 +13,9 @@ width=30
 height=60
 velocity=10 #speed of our character
 
+is_jump=False
+jump_count=10
+
 run=True
 while run is True:
     pygame.time.delay(100)
@@ -26,12 +29,26 @@ while run is True:
     move_keys=pygame.key.get_pressed()
     if move_keys[pygame.K_LEFT] and x>velocity:
         x-=velocity
-    elif move_keys[pygame.K_RIGHT] and x<screen_border-width-velocity:
+    elif move_keys[pygame.K_RIGHT] and x<screen_border-width-velocity: #substracting by width moves our border making the character crossing attempts impossible
         x+=velocity
-    elif move_keys[pygame.K_UP] and y > velocity:
-        y-=velocity
-    elif move_keys[pygame.K_DOWN] and y < screen_border-height-velocity:
-        y+=velocity
+    if not(is_jump):
+        if move_keys[pygame.K_UP] and y > velocity:
+            y-=velocity
+        elif move_keys[pygame.K_DOWN] and y < screen_border-height-velocity:
+            y+=velocity
+        elif move_keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count>=-10: #jumping is moving character by ceratin amount of pixels
+            neg=1
+            if jump_count<0:
+                neg=-1
+            y-=(jump_count**2)/2*neg #how many pxiels will our character move
+            jump_count-=1
+            #aaa
+        else:
+            is_jump = False
+            jump_count=10
 
     window.fill((0,0,0))
     #creating character
