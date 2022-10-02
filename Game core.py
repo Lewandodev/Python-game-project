6a49b8121769dd451b6cd25b6ccbdcb833d3179e
Lewandodev <mlewandowski380@gmail.com>
2022-10-02 23:57:56 +0200

    Working with projectiles and the side our character is looking at

--- Game core.py
+++ Game core.py
@@ -4,8 +4,9 @@ pygame.init() #initialising pygame
 
 window=pygame.display.set_mode((600,480)) #displaying the window
 
-pygame.display.set_caption("Python Game Project")
+pygame.display.set_caption("Python Game Project") #displaying games title
 
+#uploading all the sprites/images to python
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 background = pygame.image.load('bgr.jpg')
@@ -15,7 +16,7 @@ clock=pygame.time.Clock()
 
 screen_border=600
 
-#getting rid of globals
+#getting rid of globals by using classes
 class player(object):
     def __init__(self,x,y,width,height):
         self.x=x
@@ -28,18 +29,40 @@ class player(object):
         self.left=False #which direction character is moving
         self.rigt=False
         self.walking_count=0
+        self.standing=True
+
     def draw(self,window):
         # creating character
         if self.walking_count + 1 >= 27:  # 9 sprites which I will display for 3 frames therefore 9*3. That's how many times we will go through the loop before it changes the image
             self.walking_count = 0  # In 27 FPS changing image every 3 loops makes the character look like its moving
-        if self.left:
-            window.blit(walkLeft[self.walking_count // 3], (self.x, self.y))
-            self.walking_count += 1
-        elif self.right:
-            window.blit(walkRight[self.walking_count // 3], (self.x, self.y))
-            self.walking_count += 1
+
+        if not(self.standing):
+            if self.left:
+                window.blit(walkLeft[self.walking_count // 3], (self.x, self.y)) #self.x/y->position we use integer divison for frames
+                self.walking_count += 1
+            elif self.right:
+                window.blit(walkRight[self.walking_count // 3], (self.x, self.y))
+                self.walking_count += 1
         else:
-            window.blit(char, (self.x, self.y))
+            #window.blit(char, (self.x, self.y))
+            #chcecking last frame/whether player is standing turned left or right
+            if self.rigt:
+                window.blit(walkRight[0],(self.x,self.y))
+            else:
+                window.blit(walkLeft[0],(self.x,self.y))
+
+#projectile which our character will shoot
+class projectile(object):
+    def __init__(self,x,y,radius,color,facing):
+        self.x = x
+        self.y = y
+        self.radius=radius
+        self.color=color
+        self.facing=facing
+        self.velocity=6*facing
+
+    def draw(self,window):
+        pygame.draw.circle(window,self.color,(self.x,self.y),self.radius)
 
 
 
@@ -66,14 +89,17 @@ while run is True:
         character.x-=character.velocity
         character.left=True
         character.right=False
-
+        #changing the stand
+        character.standing=False
     elif move_keys[pygame.K_RIGHT] and character.x<screen_border-character.width-character.velocity: #substracting by width moves our border making the character crossing attempts impossible
         character.x+=character.velocity
         character.left = False
         character.right = True
     else:
-        character.right=False
-        character.left=False
+        #we need to get rid of reseting rigt/left values because we wont know which way our character is looking
+        #character.right=False
+        #character.left=False
+        character.standing=True
         character.walking_count=0
 
     if not(character.is_jump):
